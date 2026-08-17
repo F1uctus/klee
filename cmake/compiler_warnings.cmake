4928f7ed1f8dfafb6e8626272961bbce8590e39a
Ilya I. Nikitin <ilya.i.nikitin@proton.me>
2026-08-17 20:35:00 +0300

    Ask clang-cl for the warnings -Wall was meant to select
    clang-cl takes the MSVC spelling of these flags, and -Wall does not mean
    there what it means to the GNU driver: it is /Wall, which is clang's
    -Weverything. That turns on -Wc++98-compat among much else, and building
    against the LLVM headers then emits hundreds of thousands of warnings --
    enough to dominate the build's wall clock and truncate a CI log before
    any real error reaches it. /W4 is the closest thing to what was wanted.
    
    (cherry picked from commit 9136ddc481bdae9464c9260a892774819d8a7d50)

--- cmake/compiler_warnings.cmake
+++ cmake/compiler_warnings.cmake
@@ -11,11 +11,21 @@
 # Compiler warnings
 ###############################################################################
 # FIXME: -Wunused-parameter fires a lot so for now suppress it.
-add_compile_options(
-  "-Wall"
-  "-Wextra"
-  "-Wno-unused-parameter"
-)
+if (CMAKE_CXX_COMPILER_FRONTEND_VARIANT STREQUAL "MSVC")
+  # clang-cl takes the MSVC spelling of these, where -Wall does not mean what it
+  # means to the GNU driver: it is /Wall, which is clang's -Weverything. That
+  # turns on -Wc++98-compat among much else, and building KLEE against the LLVM
+  # headers then emits hundreds of thousands of warnings -- enough to dominate
+  # the build's wall-clock and truncate CI logs. /W4 is the closest equivalent
+  # to what -Wall -Wextra is meant to select here.
+  add_compile_options("/W4" "/wd4100")
+else()
+  add_compile_options(
+    "-Wall"
+    "-Wextra"
+    "-Wno-unused-parameter"
+  )
+endif()
 
 ###############################################################################
 # Warnings as errors
